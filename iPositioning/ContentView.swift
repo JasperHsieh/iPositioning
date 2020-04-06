7d5ec6e6f0e97c14fb062fb1857193fd7c34c2ff
Jasper Hsieh <jasperhsieh666@gmail.com>
2020-04-05 21:40:06 -0700

    Initial Commit

---
+++ iPositioning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPositioning
+//
+//  Created by Jasper Hsieh on 4/5/20.
+//  Copyright © 2020 Jasper Hsieh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
