b4385c9c7e0f5fd62dbc935b689bd3348ba43c65
Jasper Hsieh <jasperhsieh666@gmail.com>
2020-04-17 15:10:09 -0700

    Change beacon image and refactor code

--- iPositioning/BleManager.swift
+++ iPositioning/BleManager.swift
@@ -16,17 +16,20 @@ class BleManager: NSObject, CBCentralManagerDelegate, ObservableObject {
     @Published var iBeaconRssi2: NSNumber = 0
     @Published var iBeaconRssi3: NSNumber = 0
 
-    @Published var iBeaconDist1: Decimal = 0
-    @Published var iBeaconDist2: Decimal = 0
-    @Published var iBeaconDist3: Decimal = 0
+    @Published var iBeaconDist1: Double = 0
+    @Published var iBeaconDist2: Double = 0
+    @Published var iBeaconDist3: Double = 0
 
     @Published var x: CGFloat = 100.0
     @Published var y: CGFloat = 100.0
 
+    let roomWidth: Double = 3.8
+    let roomLength: Double = 6.4
+
     let beacon1 = "myBeacon1"
     let beacon2 = "myBeacon2"
     let beacon3 = "myBeacon3"
-    let target = 1
+    let target = 3
 
     var beaconFound = 0
 
@@ -52,26 +55,26 @@ class BleManager: NSObject, CBCentralManagerDelegate, ObservableObject {
         if isOurBeacon(device: peripheral) {
             print("\nName   : \(peripheral.name ?? "(No name)")")
             print("RSSI   : \(RSSI)")
+            let dist = getDistance(rssi: RSSI, txPower: -64)
+            print("Distance: \(dist)")
             beaconFound += 1
+
             if peripheral.name == beacon1 {
                 iBeaconRssi1 = RSSI
-                iBeaconDist1 = getDistance(rssi: RSSI, txPower: -64)
+                iBeaconDist1 = dist
             } else if peripheral.name == beacon2 {
                 iBeaconRssi2 = RSSI
-                iBeaconDist2 = getDistance(rssi: RSSI, txPower: -64)
+                iBeaconDist2 = dist
             } else if peripheral.name == beacon3 || peripheral.name == "J_iBcn"{
                 iBeaconRssi3 = RSSI
-                iBeaconDist3 = getDistance(rssi: RSSI, txPower: -64)
-                print("Distance: \(iBeaconDist3)")
-    //            for ad in advertisementData {
-    //                print("AD Data: \(ad)")
-    //            }
+                iBeaconDist3 = dist
             }
             if beaconFound == target {
                 // Update location
                 print("Found all beacons")
-                self.x = self.x + 10
-                self.y = self.y + 10
+                var coords = getCoordinates(width: roomWidth, length: roomLength, d2: iBeaconDist2, d3: iBeaconDist3, d1: iBeaconDist1)
+                print("(\(coords[0]), \(coords[1]))")
+
             }
         }
     }
@@ -83,24 +86,29 @@ class BleManager: NSObject, CBCentralManagerDelegate, ObservableObject {
         return false
     }
 
-    func getDistance(rssi: NSNumber, txPower: Int)-> Decimal{
+    func getDistance(rssi: NSNumber, txPower: Int)-> Double{
         let tmp1 = -64.0 - Double(truncating: rssi)
         let tmp2 = tmp1 / (10*2)
         //print("\(tmp1) \(tmp2)")
         let distance = Double(round(pow(10, tmp2)*100)/100)
-        return Decimal(distance)
+       // return Decimal(distance)
+        return distance
     }
     
-    func getCoordinates(width: Double, length: Double, a: Double, b: Double, c: Double) -> [Double] {
+    func getCoordinates(width: Double, length: Double, d2 a: Double, d3 b: Double, d1 c: Double) -> [Double] {
+        print(width, length, a, b, c)
+
         let p1 = (length + a + c) / 2.0
         let s1 = sqrt(p1 * (p1 - a) * (p1 - c) * (p1 - length))
         let h1 = 2.0 * s1 / length
         let y = sqrt(c * c -  h1 * h1)
-        
+        print("p1:\(p1) s1:\(s1) h1:\(h1) y:\(y)")
+
         let p2 = (width + b + c) / 2.0
         let s2 = sqrt(p2 * (p2 - b) * (p2 - c) * (p2 - width))
         let h2 = 2.0 * s2 / width
         let x = sqrt(c * c - h2 * h2)
+        print("p2:\(p2) s2:\(s2) h2:\(h2) x:\(x)")
         return [x, y]
     }
 }
--- iPositioning/ContentView.swift
+++ iPositioning/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             VStack(alignment: .leading) {
-                Image("Beacon")
+                Image("b2")
                     .resizable()
                     .frame(width: 50.0, height: 50.0)
                 Spacer()
@@ -26,11 +26,11 @@ struct ContentView: View {
                     .frame(width: 20.0, height: 20.0)
                     .position(x: bleManager.x, y: bleManager.y)
                 HStack{
-                    Image("Beacon")
+                    Image("b1")
                         .resizable()
                         .frame(width: 50.0, height: 50.0)
                     Spacer()
-                    Image("Beacon")
+                    Image("b3")
                         .resizable()
                         .frame(width: 50.0, height: 50.0)
                 }
@@ -39,9 +39,9 @@ struct ContentView: View {
             .background(colorScheme == .dark ? Color.white : Color.black)
             .coordinateSpace(name: "Custom")
 
-            Text("Beacon 1 : (\(bleManager.iBeaconRssi1.stringValue), \(NSDecimalNumber(decimal: bleManager.iBeaconDist1).stringValue) m)")
-            Text("Beacon 2 : (\(bleManager.iBeaconRssi2.stringValue), \(NSDecimalNumber(decimal: bleManager.iBeaconDist2).stringValue) m)")
-            Text("Beacon 3 : (\(bleManager.iBeaconRssi3.stringValue), \(NSDecimalNumber(decimal: bleManager.iBeaconDist3).stringValue) m)")
+            Text("Beacon 1 : (\(bleManager.iBeaconRssi1.stringValue), \(bleManager.iBeaconDist1) m")
+            Text("Beacon 2 : (\(bleManager.iBeaconRssi2.stringValue), \(bleManager.iBeaconDist2) m")
+            Text("Beacon 3 : (\(bleManager.iBeaconRssi3.stringValue), \(bleManager.iBeaconDist3) m")
             Button(action: {
                 print("Click Scan")
                 self.bleManager.startScan()
